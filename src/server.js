5751c576007bc66a19f5984b08359cc012a793a0
RushingAlien <rushing27alien@gmail.com>
2024-06-21 11:10:48 +0700

    add dockerfile

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const storage = new Storage({
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
-    host: process.env.HOST || 'localhost',
+    host: process.env.HOST || '0.0.0.0',
     routes: {
       cors: true,
     },
